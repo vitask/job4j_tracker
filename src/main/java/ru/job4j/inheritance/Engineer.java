ce4f026dc8f4b54cd7af4b7567c7305d404a7c8b
vitask <skalenko.v@yandex.ru>
2022-06-20 16:37:36 +0700

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Engineer extends Profession {
+
+    private int experience;
+
+    public Engineer(boolean degree, int experience) {
+        super(degree);
+        this.experience = experience;
+    }
+}
