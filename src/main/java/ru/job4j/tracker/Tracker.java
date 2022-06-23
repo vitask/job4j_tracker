ff4b487b264c00017a34fa335bdbf389275fa2eb
vitask <skalenko.v@yandex.ru>
2022-06-23 20:56:58 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemResult = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                itemResult[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemResult, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemResult = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                if (key.equals(item.getName())) {
+                    itemResult[size] = item;
+                    size++;
+                }
+            }
+        }
+        return Arrays.copyOf(itemResult, size);
     }
 }
