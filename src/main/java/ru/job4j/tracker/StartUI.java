ca3520e56f9fe85bd6e6a8f976059275d29ebaea
vitask <skalenko.v@yandex.ru>
2022-06-27 15:56:50 +0700

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #316067]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,7 +12,7 @@ public class StartUI {
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
                 System.out.println("== Create a new Item ===");
-                System.out.println("Enter name: ");
+                System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
