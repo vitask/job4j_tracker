7a79abc65545c3d6fb4e401b4e6cd2a91da72f1e
vitask <skalenko.v@yandex.ru>
2022-06-28 20:47:51 +0700

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -48,6 +48,16 @@ public class StartUI {
                 } else {
                     System.out.println("Removal error. Try again.");
                 }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Item with " + id + " id not found. Try again.");
+                }
             } else if (select == 6) {
                 run = false;
             } else {
