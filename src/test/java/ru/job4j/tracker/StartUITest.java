b61b286d9198caaccd8f2f7b7402f3bbc4fdf8ff
vitask <skalenko.v@yandex.ru>
2022-07-02 14:49:21 +0700

    5. Input и полиморфизм. [#181094]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+
+    @Test
+    public void whenAddItem() {
+        String[] answer = {"Fix PC"};
+        Input input = new StubInput(answer);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
+    }
+}
