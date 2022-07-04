a47cf3be05fd72903cf1c29aa936f833cf41eb67
vitask <skalenko.v@yandex.ru>
2022-07-04 12:44:20 +0700

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #318335]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,13 +2,10 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        int res;
         if (n == 0 || n == 1) {
-            res = 1;
-        } else {
-            res = calc(n - 1) * n;
+            return 1;
         }
-        return res;
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
