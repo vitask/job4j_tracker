dfa81b5d15b5e642af38b48499eae43c77618cba
vitask <skalenko.v@yandex.ru>
2022-07-24 23:52:38 +0700

    2.0. Лямбда [#58165]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Oleg", "Stepan", "Seven", "Anna", "Kolya");
+        System.out.println(name);
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        name.sort(cmpText);
+        System.out.println(name);
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(right.length(), left.length());
+        name.sort(cmpDescSize);
+        System.out.println(name);
+    }
+}
