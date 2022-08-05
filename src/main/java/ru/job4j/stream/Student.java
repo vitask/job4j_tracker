28b249bb0842572b54f7fdf8250368c224764b3c
vitask <skalenko.v@yandex.ru>
2022-08-05 23:14:46 +0700

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,38 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Student {
+    private int score;
+    private String name;
+
+    public Student(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, name);
+    }
+}
